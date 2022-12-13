37b3aa36064443695d1d33322254f414c045fd9b
NgasirohNurjayatri <112462000+NgasirohNurjayatri@users.noreply.github.com>
2022-12-13 10:40:01 +0700

    Add files via upload

---
+++ Intructions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	std::cout<<"+Intructions+\n";
+	cout<<"==============\n\n";
+	
+	std::cout<<"Selamat datang di game yang bertemakan menambang mineral\n";
+	std::cout<<"tugas anda sebagai pemain adalah mengumpulkan seluruh\n";
+	std::cout<<"mineral yang ada diseluruh lantai pada stope.\n\n\n";
+	
+	std::cout<<"+Characters+\n";
+	cout<<"==============\n\n";
+	
+	std::cout<<"X = Mineral -> (Dimana kalian bisa mendapatkan Stone, Gold, Cooper)\n\n";
+	std::cout<<"P = Anda\n\n";
+	std::cout<<"# = Tangga\n\n";
+	
+	std::cout<<"+How To Play+\n";
+	cout<<"==============\n\n";
+	
+	std::cout<<"Cara memainkan game ini yaitu dengan mengarahkan P (character player)\n";
+	std::cout<<"dengan menggunakan shourcut keyboard berikut\n\n";
+	
+	std::cout<<"W = Atas, S = Bawah, A = Kiri, D = Kanan\n\n";
+	
+	std::cout<<"Jika sudah mengerti, let's play the Game!!\n\n";
+	
+}
